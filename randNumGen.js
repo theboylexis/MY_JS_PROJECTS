917af3c7b2ef64a83e7ebb45d178969860e1f008
Alex Marfo Appiah <alexmarfo509@gmail.com>
2025-07-21 12:42:03 +0100

    Update randNumGen.js

--- randNumGen.js
+++ randNumGen.js
@@ -5,3 +5,6 @@ console.log(numBtnZeroAndHundred) //This genertates numbers between 0 and 99.999
 
 let randomNumRoundToFloor = Math.floor(numBtnZeroAndHundred) //Rounds max value to least whole
 console.log(randomNumRoundToFloor) //This generates numbers between 0 and 100
+
+let randomXToY = Math.floor(Math.random() * (Y - X + 1) + X; 
+console.log(randomXToY) //This generates random numbers between two chosen values
